fbba8a5944ee546ef0f2fd6eb205787bbf2cc77f
Tobil <ade.suhada@mail.ugm.ac.id>
2026-01-20 12:52:11 +0700

    fix: dbui

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -3,9 +3,12 @@ return {
     "kristijanhusak/vim-dadbod-ui",
     dependencies = {
       { "tpope/vim-dadbod", lazy = false },
-      { "kristijanhusak/vim-dadbod-completion", lazy = false },
+      {
+        "kristijanhusak/vim-dadbod-completion",
+        ft = { "sql", "mysql", "plsql" },
+        lazy = false,
+      },
     },
-    -- Mematikan lazy loading agar command :DBUI langsung tersedia
     lazy = false,
     keys = {
       { "<leader>du", "<cmd>DBUIToggle<cr>", desc = "Toggle Dadbod UI" },
@@ -14,14 +17,11 @@ return {
       { "<leader>dl", "<cmd>DBUILastQueryInfo<cr>", desc = "Last Query Info" },
     },
     init = function()
-      -- Konfigurasi tampilan UI
       vim.g.db_ui_use_nerd_fonts = 1
       vim.g.db_ui_show_help = 0
       vim.g.db_ui_win_width = 30
       vim.g.db_ui_force_echo_execute = 1
-
-      -- Lokasi penyimpanan query favorit/history
-      -- Pastikan folder ini ada atau akan dibuat otomatis
+      vim.g.db_ui_execute_on_save = 0
       vim.g.db_ui_save_location = vim.fn.stdpath("config") .. "/db_queries"
     end,
   },
